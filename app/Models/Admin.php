f1dd924bd53f563777058628fb7c4e5c13971730
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2026-03-14 00:37:33 +0500

    Add admin authentication provider and seed initial admin user

---
+++ app/Models/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable implements FilamentUser
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',    
+        'password'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Admin;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Admin::create([
+            'name'=>'Admin',
+            'email'=>'nasiba@imel.uz',
+            'password'=>bcrypt('admin@1234')
         ]);
     }
 }
